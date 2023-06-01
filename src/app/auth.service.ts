492ff9449554fcf524d76d8080e1a7802f1dc410
thiaug <60008101+thiaug@users.noreply.github.com>
2023-05-31 23:20:28 -0300

    2 files modified

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
+import { user } from '@angular/fire/auth';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import firebase from 'firebase/compat/app';
 
 @Injectable()
 export class AuthService {
+  // public isLoggedIn: boolean;
   user: any;
   error: any;
 
-  constructor(public auth: AngularFireAuth) {}
+  constructor(public auth: AngularFireAuth) {
+    // this.isLoggedIn = false;
+  }
 
   async emailSignin(email: string, password: string) {
     try {
@@ -17,6 +21,7 @@ export class AuthService {
       );
       this.user = credential.user;
       this.error = null;
+      // this.isLoggedIn = true;
     } catch (error) {
       this.error = error;
     }
@@ -28,6 +33,7 @@ export class AuthService {
       const credential = await this.auth.signInWithPopup(provider);
       this.user = credential.user;
       this.error = null;
+      // this.isLoggedIn = true;
     } catch (error) {
       this.error = error;
     }
@@ -36,5 +42,10 @@ export class AuthService {
   async signOut() {
     await this.auth.signOut();
     this.user = null;
+    // this.isLoggedIn = false;
+  }
+
+  async sessionSave() {
+    // sessionStorage.setItem(this.isLoggedIn, true);
   }
 }

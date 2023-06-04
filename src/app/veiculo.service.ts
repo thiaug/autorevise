2cafe0c4828929c57f505c66ec61960223c9bf4e
thiaug <60008101+thiaug@users.noreply.github.com>
2023-06-04 15:58:24 -0300

    2 files modified

--- src/app/veiculo.service.ts
+++ src/app/veiculo.service.ts
@@ -11,6 +11,7 @@ import { Veiculo } from './veiculo';
 @Injectable({
   providedIn: 'root',
 })
+
 export class VeiculoService {
   private veiculosCollection: AngularFirestoreCollection<Veiculo>;
 
--- src/app/veiculo/veiculo.component.ts
+++ src/app/veiculo/veiculo.component.ts
@@ -11,53 +11,57 @@ import { Veiculo } from '.././veiculo';
 export class VeiculoComponent implements OnInit {
   veiculos: Veiculo[] = [];
 
-  constructor(public veiculoService: VeiculoService) {}
-  // ERRO AO CHAMAR O CONSTRUTOR !!!!
+  constructor() {}
+  // ERRO AO CHAMAR SERVICO NO CONSTRUTOR !!!!
 
   ngOnInit() {
-    this.loadVeiculos();
+    // this.loadVeiculos();
   }
 
-  loadVeiculos() {
-    this.veiculoService.getAllVeiculos().subscribe((changes) => {
-      this.veiculos = changes.map((action) => {
-        const data = action.payload.doc.data() as Veiculo;
-        data.id = action.payload.doc.id;
-        return data;
-      });
-    });
-  }
+  // ----------------- CARREGA -----------------
+  // loadVeiculos() {
+  //   this.veiculoService.getAllVeiculos().subscribe((changes) => {
+  //     this.veiculos = changes.map((action) => {
+  //       const data = action.payload.doc.data() as Veiculo;
+  //       data.id = action.payload.doc.id;
+  //       return data;
+  //     });
+  //   });
+  // }
 
-  addVeiculo(veiculo: Veiculo) {
-    this.veiculoService
-      .addVeiculo(veiculo)
-      .then(() => {
-        console.log('TESTE: Veiculo add');
-      })
-      .catch((error) => {
-        console.error('Erro ao adicionar veículo', error);
-      });
-  }
+  // ----------------- ADICIONA -----------------
+  // addVeiculo(veiculo: Veiculo) {
+  //   this.veiculoService
+  //     .addVeiculo(veiculo)
+  //     .then(() => {
+  //       console.log('TESTE: Veiculo add');
+  //     })
+  //     .catch((error) => {
+  //       console.error('Erro ao adicionar veículo', error);
+  //     });
+  // }
 
-  updateVeiculo(veiculo: Veiculo) {
-    this.veiculoService
-      .updateVeiculo(veiculo)
-      .then(() => {
-        console.log('TESTE: Veiculo Atualizado!');
-      })
-      .catch((error) => {
-        console.error('Erro ao atualizar veículo', error);
-      });
-  }
+  // ----------------- UPDATE -----------------
+  // updateVeiculo(veiculo: Veiculo) {
+  //   this.veiculoService
+  //     .updateVeiculo(veiculo)
+  //     .then(() => {
+  //       console.log('TESTE: Veiculo Atualizado!');
+  //     })
+  //     .catch((error) => {
+  //       console.error('Erro ao atualizar veículo', error);
+  //     });
+  // }
 
-  deleteVeiculo(id: string) {
-    this.veiculoService
-      .deleteVeiculo(id)
-      .then(() => {
-        console.log('TESTE: Veiculo Deletado');
-      })
-      .catch((error) => {
-        console.error('Erro ao deletar veiculo');
-      });
-  }
+  // ----------------- DELETE -----------------
+  // deleteVeiculo(id: string) {
+  //   this.veiculoService
+  //     .deleteVeiculo(id)
+  //     .then(() => {
+  //       console.log('TESTE: Veiculo Deletado');
+  //     })
+  //     .catch((error) => {
+  //       console.error('Erro ao deletar veiculo');
+  //     });
+  // }
 }
